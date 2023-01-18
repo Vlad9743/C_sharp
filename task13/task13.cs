8bd9e749a5e8b11ebbaa708f1d87a08f84d6b7cc
Vlad <blackboxorange@gmail.com>
2023-01-18 17:28:24 +0400

    task13_rev1

---
+++ task13/task13.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Inpun number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int lastDigit = 0;
+
+if (num < 100)
+{
+    Console.WriteLine("There is no third digit.");
+}
+
+else
+{
+    while (num > 100)
+    {
+        lastDigit = num % 10;
+        num /= 10;
+    }
+    Console.WriteLine("The third digit is: " + lastDigit);
+}
