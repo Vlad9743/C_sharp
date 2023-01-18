0bdd7612f0b83b980f85fadfa16ffea5f8651f04
Vlad <blackboxorange@gmail.com>
2023-01-18 17:27:04 +0400

    task10_rev1

---
+++ task10/task10.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Input 3-digit number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Wrong input.");
+}
+else
+{
+num /=10;
+//Console.WriteLine(num);
+
+num %= 10;
+Console.WriteLine("Second digit is: " + num);
+}
