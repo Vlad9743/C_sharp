9a83d45580a392c648bb67a04861593d1b13bf72
Vlad <blackboxorange@gmail.com>
2023-01-18 19:24:25 +0400

    task15_rev2

--- task15/task15.cs
+++ task15/task15.cs
@@ -5,7 +5,11 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 void holidayChecker(int dayNum)
 {
-    if (dayNum == 6 || dayNum ==7)
+    if (dayNum < 1 || dayNum >7)
+    {
+        Console.WriteLine("Wrong Input");
+    }
+    else if (dayNum == 6 || dayNum ==7)
     {
         Console.WriteLine("It's a holiday!!!");
     }
