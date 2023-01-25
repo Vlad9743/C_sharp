917127ac2505490b4b587c8620d64d06e154c029
Vlad <blackboxorange@gmail.com>
2023-01-25 18:24:08 +0400

    task27_rev0

---
+++ task27/task27.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Input number: ");
+int inputNum = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDigits(int num)
+{
+    num = Math.Abs(num);//чтобы цикл сработал для отрицательный чисел
+
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.Write("The sum of the digits in" + inputNum + " is equal to ");
+Console.WriteLine(SumOfDigits(inputNum));
