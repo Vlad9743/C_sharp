8c70876d18adae88dc533d95dbc4276938883f16
Vlad <blackboxorange@gmail.com>
2023-02-09 20:51:45 +0400

    task60_rev0

---
+++ task60/task60.cs
@@ -0,0 +1,46 @@
+﻿//Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+int[,,] Array3dBuilder(int dimentionX, int dimentionY, int dimentionZ)
+{
+    int[,,] builtArray = new int[dimentionX, dimentionY, dimentionZ];
+    int start = 0;
+    
+    for (int i = 0; i < dimentionX; i++)
+    {
+        for (int j = 0; j < dimentionY; j++)
+        {
+            for (int k = 0; k < dimentionZ; k++)
+            {
+                builtArray[i,j,k] = start;
+                start++;
+            }
+        }
+    }
+
+    return builtArray;
+}
+
+void Print3dArray(int[,,] originalArray)
+{
+    for (int i = 0; i < originalArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < originalArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < originalArray.GetLength(2); k++)
+            {
+                Console.Write("|"+originalArray[i,j,k]+"("+i+","+j+","+k+")|");
+            }
+            Console.WriteLine();
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
+
+//main
+int arrayDimention1 = 5;
+int arrayDimention2 = 2;
+int arrayDimention3 = 7;
+
+int[,,] newArray = Array3dBuilder(arrayDimention1, arrayDimention2, arrayDimention3);
+Print3dArray(newArray);
