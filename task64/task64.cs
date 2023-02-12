385c08cc2cd6c2d5a360c34f80617b475c49da14
Vlad <blackboxorange@gmail.com>
2023-02-12 11:28:51 +0400

    task64_rev0

---
+++ task64/task64.cs
@@ -0,0 +1,14 @@
+﻿//Задача 64: Задайте значение N.
+//Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int MinusOne(int number)
+{
+    Console.Write(number + ", ");
+    if (number == 1) return 1;
+    return MinusOne(number-1);
+}
+
+//main
+Console.Write("Input N:");
+int num = Convert.ToInt32(Console.ReadLine());
+MinusOne(num);
